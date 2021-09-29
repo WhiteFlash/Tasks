20ca7ac3884218a392cf3d48b40e4891709dc268
E1 <ivankotov375@gmail.com>
2021-09-29 21:33:06 +0300

    Замена Service на Interfaces для семантического значения Моделей.

---
+++ Server/Tasks/Tasks.Core/Interfaces/IStatus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tasks.Core.Interfaces
+{
+    interface IStatus
+    {
+        public string StatusTypes { get; set; }
+    }
+}
---
+++ Server/Tasks/Tasks.Core/Interfaces/ITasks.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tasks.Core.Interfaces
+{
+    interface ITasks
+    {
+        public string TasksName { get; set; }
+    }
+}
